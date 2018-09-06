ab51831f84be3b81108b7bb58b8fba89d2a61c9f
Renzo VAN WYK <revan-wy@c2r6s4.wethinkcode.co.za>
2018-09-06 22:54:40 +0200

    why doesn't my window open

---
+++ wolf.c
@@ -0,0 +1,188 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: revan-wy <revan-wy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/03 17:49:45 by revan-wy          #+#    #+#             */
+/*   Updated: 2018/09/06 22:52:26 by revan-wy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf.h"
+
+/*int key_event(int key_code, void **param)
+{
+	param = 0;
+	
+	if (key_code == 53)
+		exit(0);
+	return (1);
+}
+
+void vert_line(int x, int drawStart, int drawEnd, int colour, void *gsci, void *win)
+{
+	while (drawStart <= drawEnd)
+	{
+		mlx_pixel_put(gsci, win, x, drawStart++, colour);
+	}
+}
+
+void	draw_screen(int worldMap[MAPWIDTH][MAPHEIGHT], void *gsci, void *win)
+{
+	int x;
+	
+	x = 0;
+	while (x < WINWIDTH)
+	{
+		double posX = 22, posY = 12; //x and y start position
+		double dirX = -1, dirY = 0;  //initial direction vector
+		double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
+		double cameraX = 2 * x / WINWIDTH - 1; //x-coordinate in camera space
+		double rayPosX = posX;
+		double rayPosY = posY;
+		double rayDirX = dirX + planeX * cameraX;
+		double rayDirY = dirY + planeY * cameraX;
+		//which box of the map we're in
+		int mapX = (int)(rayPosX);
+		int mapY = (int)(rayPosY);
+		//length of ray from current position to next x or y-side
+		double sideDistX;
+		double sideDistY;
+		//length of ray from one x or y-side to next x or y-side
+		double deltaDistX = sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
+		double deltaDistY = sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
+		double perpWallDist;
+		//what direction to step in x or y-direction (either +1 or -1)
+		int stepX;
+		int stepY;
+		int hit = 0; //was there a wall hit?
+		int side; //was a NS or a EW wall hit?
+		//calculate step and initial sideDist
+		if (rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (rayPosX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - rayPosX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (rayPosY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - rayPosY) * deltaDistY;
+		}
+		//perform DDA
+		while (hit == 0)
+		{
+			//jump to next map square, OR in x-direction, OR in y-direction
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			//Check if ray has hit a wall
+			if (worldMap[mapX][mapY] > 0) hit = 1;
+		}
+		//Calculate distance projected on camera direction (oblique distance will give fisheye effect!)
+		if (side == 0)
+		perpWallDist = fabs((mapX - rayPosX + (1 - stepX) / 2) / rayDirX);
+		else
+		perpWallDist = fabs((mapY - rayPosY + (1 - stepY) / 2) / rayDirY);
+		//Calculate height of line to draw on screen
+		int lineHeight = abs((int)(WINHEIGHT / perpWallDist));
+		//calculate lowest and highest pixel to fill in current stripe
+		int drawStart = -lineHeight / 2 + WINHEIGHT / 2;
+		if (drawStart < 0) drawStart = 0;
+		int drawEnd = lineHeight / 2 + WINHEIGHT / 2;
+		if (drawEnd >= WINHEIGHT) drawEnd = WINHEIGHT - 1;
+		//choose wall color
+		
+		int colour; //build colour chooser
+		
+		//if (worldMap[mapX][mapY] == 1)
+			colour = 0x00FF0000;
+		//else if 
+		
+		
+		ColorRGB color;
+		switch(worldMap[mapX][mapY])
+		{
+			case 1:  color = RGB_Red;  break; //red
+			case 2:  color = RGB_Green;  break; //green
+			case 3:  color = RGB_Blue;   break; //blue
+			case 4:  color = RGB_White;  break; //white
+			default: color = RGB_Yellow; break; //yellow
+		}*//*
+		
+		
+		//give x and y sides different brightness
+		if (side == 1) 
+			colour = colour / 2;
+		//draw the pixels of the stripe as a vertical line
+		vert_line(x, drawStart, drawEnd, colour, gsci, win);
+		x++;
+	}
+}
+*/
+int	main()
+{
+	//void *gsci;
+	//void *win;
+
+	//gsci = mlx_init();
+	//win = mlx_new_window(gsci, 2560, 1315, "#MAKEMEHOWL");
+	
+	
+
+	/*int worldMap[MAPWIDTH][MAPHEIGHT]=
+	{
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};*/
+	
+	//draw_screen(worldMap, gsci, win);
+
+	while(1);
+		
+	//draw screen //this will be the initial starting view of the camera
+	//mlx_key_hook(win, key_event, 0);
+	//mlx_loop(gsci);
+}
--- wolf.h
+++ wolf.h
@@ -6,7 +6,7 @@
 /*   By: revan-wy <revan-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 17:50:27 by revan-wy          #+#    #+#             */
-/*   Updated: 2018/09/03 17:51:38 by revan-wy         ###   ########.fr       */
+/*   Updated: 2018/09/06 22:20:07 by revan-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 # define WOLF_H
 
 # include "mlx.h"
+# include <stdlib.h>
+# include <math.h>
 
 # define WINWIDTH 640
 # define WINHEIGHT 420
+# define MAPWIDTH 24
+# define MAPHEIGHT 24
+
 
 
 #endif
