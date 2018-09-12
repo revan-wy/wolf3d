876c3f543e07eb08784523e8a2ae81d1a3411cbc
Renzo VAN WYK <revan-wy@c2r6s4.wethinkcode.co.za>
2018-09-12 21:17:42 +0200

    achieved rendering of initial game state

--- wolf.c
+++ wolf.c
@@ -6,7 +6,7 @@
 /*   By: revan-wy <revan-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 09:51:43 by revan-wy          #+#    #+#             */
-/*   Updated: 2018/09/11 21:02:34 by revan-wy         ###   ########.fr       */
+/*   Updated: 2018/09/12 21:15:56 by revan-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,8 @@
 t_data *init_t_data()
 {
 	t_data *ret;
-	//int element_count = 8;
-	int i;
 	
 	ret = ft_memalloc(sizeof(t_data));
-	i = 1;
-	/*ret->gsci = ft_memalloc(sizeof(double) * element_count);
-	ret->win = ret->gsci + i++;
-	ret->posX = ret->gsci + i++;
-	ret->posY = ret->gsci + i++;
-	ret->dirX = ret->gsci + i++;
-	ret->dirY = ret->gsci + i++;
-	ret->planeX = ret->gsci + i++;
-	ret->planeY = ret->gsci + i++;*/
-	printf("Element count in struct is %d\n", i);
 	return (ret);
 }
 
@@ -39,9 +27,46 @@ void set_up_window(t_data *data)
 	data->win = mlx_new_window(data->gsci, win_width, win_height, "#makemehowl");
 }
 
+void draw_vert_line(t_data *data)
+{
+	int y;
+	
+	y = data->drawStart;
+	while (y <= data->drawEnd)
+		mlx_pixel_put(data->gsci,data->win,data->x,y++,data->colour);
+}
+
 int main()
 {
 	t_data *data;
+	int worldMap[mapWidth][mapHeight]=
+	{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+	};
+
 	
 	data = init_t_data();
 	
@@ -54,18 +79,100 @@ int main()
 	
 	set_up_window(data);
 	
-	
-	printf("%f\n", data->posX);
-	printf("%f\n", data->posY);
-	printf("%f\n", data->dirX);
-	printf("%f\n", data->dirY);
-	printf("%f\n", data->planeX);
-	printf("%f\n", data->planeY);
-	printf("%lu\n", sizeof(double));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(float));
-	printf("%lu\n", sizeof(void*));
-	while(1);
+	data->x = 0;
+	while(data->x < win_width)
+	{
+		data->cameraX = 2 * data->x / (double)win_width - 1;
+		data->rayPosX = data->posX;
+		data->rayPosY = data->posY;
+		data->rayDirX = data->dirX + data->planeX * data->cameraX;
+		data->rayDirY = data->dirY + data->planeY * data->cameraX;
+		printf("%f\n", data->cameraX);
+		
+		data->mapX = data->rayPosX;
+		data->mapY = data->rayPosY;
+		
+		data->deltaDistX = sqrt(1 + (data->rayDirY * data->rayDirY) / (data->rayDirX * data->rayDirX));
+		data->deltaDistY = sqrt(1 + (data->rayDirX * data->rayDirX) / (data->rayDirY * data->rayDirY));
+		
+		data->hit = 0;
+		
+		if (data->rayDirX < 0)
+		{
+			data->stepX = -1;
+			data->sideDistX = (data->rayPosX - data->mapX) * data->deltaDistX;
+		}
+		else
+		{
+			data->stepX = 1;
+			data->sideDistX = (data->mapX + 1.0 - data->rayPosX) * data->deltaDistX;
+		}
+		if (data->rayDirY < 0)
+		{
+			data->stepY = -1;
+			data->sideDistY = (data->rayPosY - data->mapY) * data->deltaDistY;
+		}
+		else
+		{
+			data->stepY = 1;
+			data->sideDistY = (data->mapY + 1.0 - data->rayPosY) * data->deltaDistY;
+		}
+
+		while (data->hit == 0)
+		{
+			//jump to next map square, OR in x-direction, OR in y-direction
+			if (data->sideDistX < data->sideDistY)
+			{
+				data->sideDistX += data->deltaDistX;
+				data->mapX += data->stepX;
+				data->side = 0;
+			}
+			else
+			{
+				data->sideDistY += data->deltaDistY;
+				data->mapY += data->stepY;
+				data->side = 1;
+			}
+			//Check if ray has hit a wall
+			if (worldMap[data->mapX][data->mapY] > 0)
+				data->hit = 1;
+		}
+
+		if (data->side == 0)
+			data->perpWallDist = fabs((data->mapX - data->rayPosX + (1 - data->stepX) / 2) / data->rayDirX);
+		else
+			data->perpWallDist = fabs((data->mapY - data->rayPosY + (1 - data->stepY) / 2) / data->rayDirY);
+
+		data->lineHeight = fabs(win_height / data->perpWallDist);
+
+		data->drawStart = -data->lineHeight / 2 + win_height / 2;
+		if (data->drawStart < 0)
+			data->drawStart = 0;
+		data->drawEnd = data->lineHeight / 2 + win_height / 2;
+		if (data->drawEnd >= win_height)
+			data->drawEnd = win_height - 1;
+
+		if (worldMap[data->mapX][data->mapY] == 1)
+			data->colour = 0x00FF0000; //red
+		else if (worldMap[data->mapX][data->mapY] == 2)
+			data->colour = 0x0000FF00; //green
+		else if (worldMap[data->mapX][data->mapY] == 3)
+			data->colour = 0x000000FF; //blue
+		else if (worldMap[data->mapX][data->mapY] == 4)
+			data->colour = 0x00FFFFFF; //white
+		else
+			data->colour = 0x00FFFF00; //yellow
+		
+		if (data->side == 1)
+			data->colour = data->colour / 2;
+
+		draw_vert_line(data);
+
+
+		
+		data->x++;
+	}
+	mlx_loop(data->gsci);
 	return (0);
 	
 }
--- wolf.h
+++ wolf.h
@@ -6,7 +6,7 @@
 /*   By: revan-wy <revan-wy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/09 15:09:16 by revan-wy          #+#    #+#             */
-/*   Updated: 2018/09/11 21:00:52 by revan-wy         ###   ########.fr       */
+/*   Updated: 2018/09/12 18:36:46 by revan-wy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,44 @@
 
 # include "libft.h"
 # include "mlx.h"
+# include <math.h>
 
 # define win_width 640
 # define win_height 460
+# define mapWidth 24
+# define mapHeight 24
 
 typedef struct  s_data
 {
-    void *gsci;
-    void *win;
-    double posX;
-    double posY;
+    double cameraX;
+    double deltaDistX;
+    double deltaDistY;
     double dirX;
     double dirY;
+    double perpWallDist;
     double planeX;
     double planeY;
+    double posX;
+    double posY;
+    double rayDirX;
+    double rayDirY;
+    double rayPosX;
+    double rayPosY;
+    double sideDistX;
+    double sideDistY;
+    int colour;
+    int drawEnd;
+    int drawStart;
+    int hit;
+    int lineHeight;
+    int mapX;
+    int mapY;
+    int side;
+    int stepX;
+    int stepY;
+    int x;
+    void *gsci;
+    void *win;
 }               t_data;
 
 #endif
